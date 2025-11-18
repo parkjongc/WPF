bb32e4d05280f9c9f1eff42a048118e343728232
lssmmn <leesumin20011103@gmail.com>
2025-11-18 09:15:40 +0900

    디비 연결 완

--- 클라/DanawaRClient/Views/UserControls/LineChart.xaml.cs
+++ 클라/DanawaRClient/Views/UserControls/LineChart.xaml.cs
@@ -1,56 +1,28 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
-using LiveCharts;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace DanawaRClient.Views.UserControls
 {
-    public partial class LineChart : UserControl, INotifyPropertyChanged
+    /// <summary>
+    /// LineChart.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LineChart : UserControl
     {
-        private ChartValues<double> _chartValues;
-        private const int MaxDataPoints = 60;
-
-        public ChartValues<double> ChartValues
-        {
-            get { return _chartValues; }
-            set
-            {
-                _chartValues = value;
-                OnPropertyChanged(nameof(ChartValues));
-            }
-        }
-
         public LineChart()
         {
             InitializeComponent();
-
-            // 초기 데이터 (60개의 0 값)
-            ChartValues = new ChartValues<double>();
-            for (int i = 0; i < MaxDataPoints; i++)
-            {
-                ChartValues.Add(0);
-            }
-
-            DataContext = this;
-        }
-
-        public void AddValue(double value)
-        {
-            // 새 값 추가
-            ChartValues.Add(value);
-
-            // 60개 초과시 가장 오래된 값 제거
-            if (ChartValues.Count > MaxDataPoints)
-            {
-                ChartValues.RemoveAt(0);
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
