1f58b017aad4b6f00df2c4b2b21f2f07cdd563bf
lssmmn <leesumin20011103@gmail.com>
2025-11-18 09:21:21 +0900

    전면적으로 클라이언트 수정.

---
+++ 클라/DanawaRClient/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using DanawaRClient.Views;
+
+namespace DanawaRClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    MainWindow = new MainWindow()
+        //    {
+        //       DataContext = new MainView()
+        //    };
+        //    MainWindow.Show();
+
+        //    base.OnStartup(e);
+        //}
+    }
+
+}
